21941ec84b6104b71cbde9627dd7b9da1f33be11
Bilal Haider <bilal-haider@outlook.com>
2018-10-17 00:07:02 +0500

    first commit

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+var krms_config ={			
+	'ApiUrl':"http://minhapadaria.com.br/mobileapp/api",	
+	'DialogDefaultTitle':"Minha Padaria",	
+	'pushNotificationSenderid':"809536782014",	
+	'facebookAppId':"2116058355315629", 
+	'APIHasKey':"msanders"
+};
